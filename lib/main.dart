c65475ab3de2dc795d324242718745074cfcc86c
Gennadii85 <103601269+Gennadii85@users.noreply.github.com>
2023-11-04 16:22:07 +0200

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,4 @@ import 'package:testjob/pages/my_app.dart';
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
-
-  /*
-  Здравствуйте. Я не уверен что правильно понял момент дизайнерского решения.
-  Если у Вас возникнут сомнения в моей компетентности - 
-  Вы можете посмотреть мой рабочий репозиторий Selina
-  */
 }
